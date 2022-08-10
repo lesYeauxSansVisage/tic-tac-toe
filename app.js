acdc4bee2f9e1b914032d7747b430e497033ec11
Gen <genesishawkeye42@gmail.com>
2022-08-10 17:11:19 -0300

    added display controller

--- app.js
+++ app.js
@@ -7,30 +7,37 @@ const Player = (sign, moves) => {
     return sign;
   };
 
-  return { getSign };
+  return { getSign, moves };
 };
 
 const gameBoard = (() => {
-  const gameboard = [];
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
 
   const BOARD_SIZE = 9;
 
   const gametable = document.querySelector("#game-table");
 
   const renderTable = () => {
-    function addCellListener(e) {
-      gameboard[+e.target.dataset.index] = "X";
-      e.target.innerText = gameController.makeMove();
-      gameController.changeTurn();
-    }
+    gametable.innerHTML = "";
 
     for (let x = 0; x < BOARD_SIZE; x++) {
+      gameboard[x] = "";
       const gameCell = document.createElement("div");
       gameCell.classList.add("game-cell");
-      gameboard.push("");
       gameCell.setAttribute("data-index", x);
       gameCell.addEventListener("click", addCellListener);
       gametable.append(gameCell);
+    }
+
+    function addCellListener(e) {
+      const currentCell = +e.target.dataset.index;
+      if (gameboard[currentCell] === "") {
+        const currentMove = gameController.makeMove(currentCell);
+        gameboard[currentCell] = currentMove;
+        e.target.innerText = currentMove;
+        gameController.checkWin();
+        gameController.changeTurn();
+      }
     }
   };
 
@@ -41,18 +48,24 @@ const gameController = (() => {
   const player1 = Player("X");
   const player2 = Player("O");
 
-  let turn = 1;
+  let turn = 0;
   let isOver = false;
 
+  let currentPlayer = player1;
+
   function changeTurn() {
+    console.log({ currentPlayer, turn });
+    currentPlayer = turn % 2 === 0 ? player1 : player2;
     turn++;
+    displayController.changeMessage(currentPlayer);
   }
 
-  function makeMove() {
-    return turn % 2 == 0 ? "X" : "O";
+  function makeMove(position) {
+    currentPlayer.moves.push(position);
+    return currentPlayer.getSign();
   }
 
-  function checkWin(sign) {
+  function checkWin() {
     const winConditions = [
       [0, 1, 2],
       [3, 4, 5],
@@ -64,16 +77,73 @@ const gameController = (() => {
       [2, 4, 6],
     ];
 
-    const playerMoves = [];
-
-    gameBoard.gameboard.forEach((cell, index) =>
-      cell === sign ? playerMoves.push(index) : console.log("Not a match")
-    );
-
-    // winConditions.forEach((conditon) => conditon.includes(...playerMoves));
+    for (let x = 0; x < winConditions.length; x++) {
+      if (
+        winConditions[x].every((move) => currentPlayer.moves.includes(move))
+      ) {
+        isOver = true;
+        showWinner();
+      }
+    }
+    showWinner();
   }
 
-  return { checkWin, changeTurn, makeMove };
+  function showWinner() {
+    if (isOver) {
+      displayController.showMessage(`Player ${currentPlayer.getSign()} won!`);
+      reset();
+    }
+
+    if (!gameBoard.gameboard.some((el) => el === "")) {
+      displayController.showMessage("It's a draw!");
+      reset();
+    }
+  }
+
+  function reset() {
+    turn = 0;
+    currentPlayer = player1;
+    player1.moves = [];
+    player2.moves = [];
+    displayController.changeMessage(player1);
+
+    isOver = false;
+    gameBoard.renderTable();
+  }
+
+  return { checkWin, changeTurn, makeMove, reset };
 })();
 
-gameBoard.renderTable();
+const displayController = (() => {
+  (function startGame() {
+    const startBtn = document.querySelector("#start");
+
+    startBtn.addEventListener("click", () => {
+      startBtn.parentElement.classList.add("up");
+      gameBoard.renderTable();
+    });
+  })();
+
+  function changeMessage(currentPlayer) {
+    const message = document.querySelector("#message-span");
+    message.innerText = currentPlayer.getSign();
+  }
+
+  function showMessage(message) {
+    const finalMessage = document.querySelector("#final-message");
+    finalMessage.parentElement.classList.add("show");
+    finalMessage.innerText = message;
+
+    setTimeout(() => {
+      finalMessage.parentElement.classList.remove("show");
+    }, 3000);
+  }
+
+  (() => {
+    const resetBtn = document.querySelector("#reset");
+
+    resetBtn.addEventListener("click", gameController.reset);
+  })();
+
+  return { changeMessage, showMessage };
+})();
